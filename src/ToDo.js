f7af3da0ee5ec6627e915f262fb65594c2fadfdd
kaankacar <kaankacar02@gmail.com>
2022-09-17 14:05:17 +0300

    finished all necessary features - need styling

--- src/ToDo.js
+++ src/ToDo.js
@@ -7,6 +7,10 @@ const ToDo = ({todo, handleToggle}) => {
         handleToggle(e.currentTarget.id)
     }
 
+    if (todo.content.length < 3){
+        return false
+    }
+
     return (
         <div id={todo.id} key={todo.id + todo.content} name="todo" value={todo.id} onClick={handleClick} className={todo.isCompleted ? "todo strike" : "todo"}>
             {todo.content}
